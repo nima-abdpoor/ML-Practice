1e3c7da0fe98628243789728226f518f98f338fd
nima-abdpoor <nimaabdi328@yahoo.com>
2021-08-15 15:30:57 +0430

    some changes.

--- project/main.py
+++ project/main.py
@@ -3,15 +3,17 @@ import pandas as pd
 melbournFilePath = 'D:/nima/python/melb_data.csv'
 melbournData = pd.read_csv(melbournFilePath)
 
+
 def drop_not_available_data():
     melbournData.dropna(axis=0)
+
 
 def print_all_columns():
     print(melbournData.columns)
 
-def get_price() : 
-    return melbournData.Price
 
+def get_price():
+    return melbournData.Price
 
 
 print(get_price())
