973e28c873d00a820799e0c7c45f476e79bf71e8
nima-abdpoor <nimaabdi328@yahoo.com>
2021-08-19 17:24:46 +0430

    predict and fit function to train and prediction.

--- project/main.py
+++ project/main.py
@@ -1,8 +1,10 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 melbournFilePath = 'D:/nima/python/melb_data.csv'
 melbournData = pd.read_csv(melbournFilePath)
 melbourn_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+melbourn_featuresTwo = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude', 'Price']
 
 
 def drop_not_available_data():
@@ -29,4 +31,14 @@ def getDataByFeatureWithHead(feature: list, number: int = 7):
     return melbournData[feature].head(number)
 
 
-print(getDataByFeatureWithHead(melbourn_features,25))
+def predict():
+    melbournModel = DecisionTreeRegressor(random_state=1)
+    print(getDataByFeatureWithHead(melbourn_features), '\n***\n')
+    print(getDataByFeatureWithHead(melbourn_featuresTwo, 100), '\n***\n')
+    print(get_price(), '\n***\n')
+    melbournModel.fit(getDataByFeature(melbourn_features), get_price())
+    print("Making predictions for the following 7 houses:\n")
+    print(melbournModel.predict(getDataByFeatureWithHead(melbourn_features)))
+
+
+print(predict())
