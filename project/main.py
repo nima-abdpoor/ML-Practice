60741cb17bb3bbeff172e312432202b62a1b062c
nima-abdpoor <nimaabdi328@yahoo.com>
2021-08-19 09:52:28 +0430

    get melbourn data by restricting it with some features. working with head and describe methods.

--- project/main.py
+++ project/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 melbournFilePath = 'D:/nima/python/melb_data.csv'
 melbournData = pd.read_csv(melbournFilePath)
+melbourn_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
 
 
 def drop_not_available_data():
@@ -16,4 +17,16 @@ def get_price():
     return melbournData.Price
 
 
-print(get_price())
+def getDataByFeature(feature: list):
+    return melbournData[feature]
+
+
+def getDataByFeatureDescribe(feature: list):
+    return melbournData[feature].describe()
+
+
+def getDataByFeatureWithHead(feature: list, number: int = 7):
+    return melbournData[feature].head(number)
+
+
+print(getDataByFeatureWithHead(melbourn_features,25))
